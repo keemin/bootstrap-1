b6c327c16ab7f5ffa30b993cfbd48a9ded1b4dca
keemin <keemin.chew.2010@sis.smu.edu.sg>
2013-03-17 15:09:28 +0800

    Update angular.js
    add image 3

--- angular.js
+++ angular.js
@@ -9,7 +9,7 @@ function CarouselDemoCtrl($scope){
   $scope.slides=[
     {image:'http://cache.gawkerassets.com/assets/images/12/2011/12/46613f8a93b052cbbd5d5f40a1690622.jpg'},
     {image:'http://zerotips.com/wp-content/uploads/2012/02/wallpaper-mclaren-f1.jpg'},
-    {image:'http://placekitten.com/250/200'},
+    {image:'http://www.osceolagarage.com/blog/wp-content/uploads/2013/02/Cars_Toon-picture-of-vehicle.jpg'},
     {image:'http://placekitten.com/275/200'}
   ];
 }
