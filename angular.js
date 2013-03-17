9c5ceec8be9d83043396b403d4d308a272f9c7bd
keemin <keemin.chew.2010@sis.smu.edu.sg>
2013-03-17 15:05:46 +0800

    Update angular.js

--- angular.js
+++ angular.js
@@ -7,7 +7,7 @@ function CollapseDemoCtrl($scope) {
 function CarouselDemoCtrl($scope){
   $scope.myInterval =2000;
   $scope.slides=[
-    {image:'http://placekitten.com/200/200'},
+    {image:'http://cache.gawkerassets.com/assets/images/12/2011/12/46613f8a93b052cbbd5d5f40a1690622.jpg'},
     {image:'http://placekitten.com/225/200'},
     {image:'http://placekitten.com/250/200'},
     {image:'http://placekitten.com/275/200'}
